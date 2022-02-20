b6d7a5624055bdbf37d60fbe6df0bbb59843d4c1
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-02-20 12:37:32 +0900

    Create bfs.cpp

---
+++ bfs.cpp
@@ -0,0 +1,19 @@
+vector<int> adj[N];
+queue<int> q;
+bool visited[N];
+int distance[N];
+
+void bfs(int x) {
+    visited[x] = true;
+    distance[x] = 0;
+    q.push(x);
+    while (!q.empty()) {
+        int s = q.front(); q.pop();
+        for (auto u: adj[s]) {
+            if (visited[u]) continue;
+            visited[u] = true;
+            distance[u] = distance[s] + 1;
+            q.push(u);
+        }
+    }
+}
